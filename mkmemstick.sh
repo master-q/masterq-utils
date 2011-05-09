c59357c448d221c82682803a9fe79244e54b34ea
Kiwamu Okabe <kiwamu@debian.or.jp>
2011-05-09 10:59:23 +0900

    build.sh support (adhoc)

--- mkmemstick.sh
+++ mkmemstick.sh
@@ -6,6 +6,15 @@
 
 src="$1"
 img="$2"
+
+objdir="/home/kiwamu/src/netbsd/current/src/obj/"
+cmd_disklabel="$objdir/tooldir.Linux-2.6.38-2-amd64-x86_64/bin/nbdisklabel-i386"
+cmd_installboot="$objdir/tooldir.Linux-2.6.38-2-amd64-x86_64/bin/nbinstallboot"
+cmd_makefs="$objdir/tooldir.Linux-2.6.38-2-amd64-x86_64/bin/nbmakefs"
+cmd_stat="$objdir/tooldir.Linux-2.6.38-2-amd64-x86_64/bin/nbstat"
+file_bootxx_ffsv1="$objdir/destdir.i386/usr/mdec/bootxx_ffsv1"
+file_mdec_boot="$objdir/destdir.i386/usr/mdec/boot"
+
 dstbase="/tmp/makeimg$(id -u)"
 dst="${dstbase}/rootfs"
 tmpimg="${dstbase}/netbsd.img"
@@ -78,21 +87,21 @@ if [ -f "${dst}/boot.cfg" ]; then
 	cat "${dstbase}/boot.cfg" | sed 's/installation CD/installation memory stick/' > "${dst}/boot.cfg"
 fi
 echo " => copying /usr/mdec/boot to $dst"
-cp /usr/mdec/boot "$dst" || die "couldn't copy /usr/mdec/boot"
+cp ${file_mdec_boot} "$dst" || die "couldn't copy /usr/mdec/boot"
 
 echo " => building filesystem image"
-makefs "$img" "$dst" || die "couldn't build filesystem image"
+${cmd_makefs} "$img" "$dst" || die "couldn't build filesystem image"
 
 echo " => installing bootxx_ffsv1"
-/usr/sbin/installboot "$img" /usr/mdec/bootxx_ffsv1 || die "couldn't install bootxx_ffsv1"
+${cmd_installboot} -m i386 "$img" ${file_bootxx_ffsv1} || die "couldn't install bootxx_ffsv1"
 
 echo " => writing disklabel"
-imgsize=$(stat -L -f %z "${img}")
+imgsize=$(${cmd_stat} -L -f %z "${img}")
 echo "$disklabel" | \
 	sed "s/@SECTORS@/$(expr $imgsize / 512)/" | \
 	sed "s/@TCYLINDERS@/$(expr $imgsize / 512 / 1000 + 1)/" | \
 	sed "s/@TSECTORS@/$(expr $imgsize / 512 + 1)/" >${dstbase}/disklabel
-disklabel -R -F "${img}" "${dstbase}/disklabel" || die "couldn't write disklabel"
+${cmd_disklabel} -R -F "${img}" "${dstbase}/disklabel" || die "couldn't write disklabel"
 
 echo " => cleaning up"
 rm -rf "${dstbase}"
