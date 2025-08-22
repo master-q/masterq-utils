51ab078b7c3201fbf0718845151a929083bb041a
Kiwamu Okabe <kiwamu@gmail.com>
2025-08-22 16:45:48 +0900

    Rev

--- flicknobody.sh
+++ flicknobody.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in {1..59}; do
+for i in {52..1}; do
     echo +++ $i +++++++++
     ret=`flickcurl people.getPublicPhotos 49071064@N00 100 $i | grep "photo with URI" | awk '{ print $6 }'`
     for j in $ret; do
