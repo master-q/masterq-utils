b5fb1f47211de2069efcf9c52295190c8785f0fd
Kiwamu Okabe <kiwamu@gmail.com>
2025-10-04 16:09:13 +0900

    Get user id on arg

--- flicknobody.sh
+++ flicknobody.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-for i in {52..1}; do
+if [ "$#" != "1" ]; then
+    echo "Usage: $0 USER-NSID"
+    exit 1
+fi
+USER_NSID=$1
+
+for i in {50..1}; do
     echo +++ $i +++++++++
-    ret=`flickcurl people.getPublicPhotos 49071064@N00 100 $i | grep "photo with URI" | awk '{ print $6 }'`
+    ret=`flickcurl people.getPublicPhotos $USER_NSID 100 $i | grep "photo with URI" | awk '{ print $6 }'`
     for j in $ret; do
         echo $j
         flickcurl photos.setPerms $j no no no "" ""
