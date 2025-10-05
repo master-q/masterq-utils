fa60a90eb96169af041fd69517ade8eb527c7fe0
Kiwamu Okabe <kiwamu@gmail.com>
2025-10-05 15:42:32 +0900

    Rename

--- flickr_set_private.sh
+++ flickr_set_private.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 if [ "$#" != "1" ]; then
-    echo "Usage: $0 USER-NSID"
+    echo "Usage: $0 USERNAME"
     exit 1
 fi
-USER_NSID=$1
+NSID=`flickcurl people.findByUsername $1 | grep -oP 'NSID\s+\K\S+(?=\s)'`
+echo $NSID
 
 for i in {50..1}; do
     echo +++ $i +++++++++
-    ret=`flickcurl people.getPublicPhotos $USER_NSID 100 $i | grep "photo with URI" | awk '{ print $6 }'`
+    ret=`flickcurl people.getPublicPhotos $NSID 100 $i | grep "photo with URI" | awk '{ print $6 }'`
     for j in $ret; do
         echo $j
         flickcurl photos.setPerms $j no no no "" ""
